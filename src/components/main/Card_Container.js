4d968c11083ac19345c1a1b9332ff3aa2eb31deb
Dave Marong <kmarong@hotmail.com>
2021-03-23 19:22:58 +0100

    CREATE -- Fetch and display api blog info

--- src/components/main/Card_Container.js
+++ src/components/main/Card_Container.js
@@ -1,5 +1,28 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
 export default function Card_Container() {
-  return <div>Bro</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:1337/posts")
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setData(result);
+      });
+  }, []);
+
+  return (
+    <div>
+      {data.map((post) => {
+        return (
+          <>
+            <h1>{post.blogtitle}</h1>
+            <p>{post.description}</p>
+            <span>{post.date}</span>
+          </>
+        );
+      })}
+    </div>
+  );
 }
---
+++ src/components/main/Post_API_Call.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Post_API_Call() {
+  return <div></div>;
+}
