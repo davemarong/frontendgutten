1ca19d70aec78d0d06c775fd55486829e2d0bee4
Dave Marong <kmarong@hotmail.com>
2021-03-23 22:02:51 +0100

    CREATE -- Dynamic post component (60%done)

--- src/components/main/Post.js
+++ src/components/main/Post.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Post() {
-  return <div></div>;
+  const [blogPost, setBlogPost] = useState({});
+
+  useEffect(() => {
+    fetch("http://localhost:1337/posts/1")
+      .then((res) => res.json())
+      .then((data) => {
+        setBlogPost(data);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>{blogPost.title}</h1>
+      <p>{blogPost.description}</p>
+      <p>{blogPost.content}</p>
+    </div>
+  );
 }
