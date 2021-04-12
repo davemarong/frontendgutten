d2a8fd96cb0eee86f8a5754ef1bbe37c3062a671
Dave Marong <kmarong@hotmail.com>
2021-04-12 11:05:33 +0200

    Add "PreviousPage" comp to "Post" page

---
+++ src/components/main/contact/Contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+import PreviousPage from "../../reUseable/PreviousPage";
+export default function Contact() {
+  return (
+    <div>
+      <PreviousPage />
+    </div>
+  );
+}
